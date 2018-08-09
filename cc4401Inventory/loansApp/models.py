11b7731a899cbe39548a12a4a1c3e2070240a3c7
Cristian Tamblay <cristian.tamblay@ing.uchile.cl>
2018-08-09 16:58:23 -0400

    Revert

--- cc4401Inventory/loansApp/models.py
+++ cc4401Inventory/loansApp/models.py
@@ -1,16 +1,8 @@
 from django.db import models
-from mainApp.models import User
-from reservationsApp.models import ReservaArticulo
-from reservationsApp.models import ReservaEspacio
 
-class PrestamoArticulo(models.Model):
-    reserva = models.ForeignKey(ReservaArticulo, on_delete=models.CASCADE)
-    administrador = models.ForeignKey(User, on_delete=models.CASCADE)
-    ESTADOS_PRES = ((1, 'Vigente'), (2, 'Caducado'), (3, 'Perdido'))
-    estado_prestamo = models.IntegerField(choices=ESTADOS_PRES, default=1)
+from articlesApp.models import Article
+from mainApp.models import Action
 
-class PrestamoEspacio(models.Model):
-    reserva = models.ForeignKey(ReservaEspacio, on_delete=models.CASCADE)
-    administrador = models.ForeignKey(User, on_delete=models.CASCADE)
-    ESTADOS_PRES = ((1, 'Vigente'), (2, 'Caducado'), (3, 'Perdido'))
-    estado_prestamo = models.IntegerField(choices=ESTADOS_PRES, default=1)
+
+class Loan(Action):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
--- cc4401Inventory/reservationsApp/models.py
+++ cc4401Inventory/reservationsApp/models.py
@@ -1,31 +1,8 @@
 from django.db import models
-from mainApp.models import Item
-from mainApp.models import User
 
-class ReservaArticulo(models.Model):
-    articulo = models.ForeignKey(Item, on_delete=models.CASCADE)
-    perfil = models.ForeignKey(User, on_delete=models.CASCADE)
-    inicio = models.DateTimeField()
-    final = models.DateTimeField()
-    ESTADOS_RES = ((1, 'Pendiente'), (2, 'Entregado'), (3, 'Rechazado'))
-    estado_reserva = models.IntegerField(choices=ESTADOS_RES, default=1)
+from mainApp.models import Action
+from spacesApp.models import Space
 
-    def __lt__(self, other):
-        return self.final < other.final
 
-    def get_cname(self):
-        class_name = self.__class__.__name__
-        return class_name
-
-class ReservaEspacio(models.Model):
-    espacio = models.ForeignKey(Item, on_delete=models.CASCADE)
-    perfil = models.ForeignKey(User, on_delete=models.CASCADE)
-    inicio = models.DateTimeField()
-    final = models.DateTimeField()
-    ESTADOS_RES = ((1, 'Pendiente'), (2, 'Entregado'), (3, 'Rechazado'))
-    estado_reserva = models.IntegerField(choices=ESTADOS_RES, default=1)
-
-    def get_cname(self):
-        class_name = self.__class__.__name__
-        return class_name
-
+class Reservation(Action):
+    space = models.ForeignKey(Space, on_delete=models.CASCADE)
